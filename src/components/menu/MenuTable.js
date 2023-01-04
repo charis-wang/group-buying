e4efc2a56ca40a946d7271742976823cf3408d20
chariwang <charis53086@gmail.com>
2023-01-04 15:31:19 +0800

    use "Grid"

--- src/components/menu/MenuTable.js
+++ src/components/menu/MenuTable.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState, useRef } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import React, { useEffect, useState, useRef } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 function createData(item, unit, price) {
   return { item, unit, price };
@@ -20,70 +20,60 @@ function createData(item, unit, price) {
 // ];
 
 const rowsTest = [
-  ['伯爵紅茶拿鐵M', '杯', 50],
-  ['伯爵紅茶拿鐵L', '杯', 60],
-  ['大正紅茶拿鐵M', '杯', 50],
-  ['大正紅茶拿鐵L', '杯', 60],
-  ['青檸香茶L', '杯', 60],
-].map(data => createData(data));
-
+  ["伯爵紅茶拿鐵M", "杯", 50],
+  ["伯爵紅茶拿鐵L", "杯", 60],
+  ["大正紅茶拿鐵M", "杯", 50],
+  ["大正紅茶拿鐵L", "杯", 60],
+  ["青檸香茶L", "杯", 60],
+].map((data) => createData(data));
 
 export default function MenuTable(props) {
   //console.log('MenuTable', props.value)
-  const [getFormValues, setGetFormValues] = useState('');
-  const [rows, setRows] = useState([])
+  const [getFormValues, setGetFormValues] = useState("");
+  const [rows, setRows] = useState([]);
   const updateGetFormValues = () => {
-    if (!props.value)
-      return
+    if (!props.value) return;
 
-    setGetFormValues(props.value)
-    // console.log('latest props', props.value)
-    setRows([...rows, props.value])
-    // console.log('latest rows', rows[rows.length - 1])
-    // console.log('rows', rows)
-  }
-
+    setGetFormValues(props.value);
+    setRows([...rows, props.value]);
+  };
 
   useEffect(() => {
     if (props.value !== null) {
-      //console.log('before rows', getRows)
-      updateGetFormValues()
-
-      //updateGetRows()
-      //console.log('after formvalues', getFormValues)
-
-      //console.log('complete rows', getRows[getRows.length - 1])
-
+      updateGetFormValues();
     }
-
-
-  }, [props.value])
+  }, [props.value]);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 200 }} aria-label="simple table">
+    <TableContainer
+      component={Paper}
+      sx={{
+        display: "flex",
+        m: 1,
+        minWidth: "100",
+        width: "80%",
+      }}
+    >
+      <Table sx={{ m: 1 }} aria-label="simple table" size="small">
         <TableHead>
           <TableRow>
             <TableCell>Item</TableCell>
 
             <TableCell align="right">Unit</TableCell>
             <TableCell align="right">Price</TableCell>
-
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
               key={row.item}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {row.item}
               </TableCell>
               <TableCell align="right">{row.unit}</TableCell>
               <TableCell align="right">{row.price}</TableCell>
-
-
             </TableRow>
           ))}
         </TableBody>
--- src/contexts/MenuCreate.js
+++ src/contexts/MenuCreate.js
@@ -1,54 +1,87 @@
 import React from "react";
-import { connect } from 'react-redux'
+import { Typography, Button, Grid, Box } from "@mui/material";
+//import { connect } from 'react-redux'
 import Navbar from "../components/Navbar";
-import BackgroundImagePage from '../components/Background';
+import BackgroundImagePage from "../components/Background";
 import MenuForm from "../components/menu/MenuForm";
-import { createMenu } from "../actions"
+//import { createMenu } from "../actions"
 import MenuTable from "../components/menu/MenuTable";
 import MenuTableForm from "../components/menu/MenuTableForm";
 
 class MenuCreate extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      formValues: null
-    }
+      formValues: null,
+      formValuesOfShop: { shopName: "ShopName" },
+    };
   }
 
   onSubmit = (value) => {
-    //console.log('before', this.state)
-    // console.log('tettttt', this.state)
     this.setState({
-      'formValues': value
-    })
-    //this.state.formValues.item = value.item
-    //this.state = { formValues: value }
-    //console.log('after', this.state)
-  }
+      formValues: value,
+    });
+  };
 
+  onSubmitOfShop = (value) => {
+    this.setState({
+      formValuesOfShop: value,
+    });
+    //console.log(this.state.formValuesOfShop)
+  };
 
   render() {
     return (
-      <div>
+      <Box>
         <Navbar />
         <BackgroundImagePage />
-        <div className="container">
-          <div className="row ">
-            <div className="col-4 ">
+        <Grid container spacing={2} mx={2} p={2}>
+          <Grid item xs={12} md={12}>
+            <Typography
+              m={0}
+              align="center"
+              variant="h5"
+              gutterBottom
+              sx={{
+                fontWeight: "bold",
+                fontStyle: "Italic",
+                color: "darkseagreen",
+              }}
+              component="h4"
+            >
+              Create New Menu
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <Typography my={1.5} mx={0.5} variant="h6" gutterBottom>
+              Create Shop
+            </Typography>
+            <MenuForm onSubmit={this.onSubmitOfShop} />
+          </Grid>
+          <Grid item xs={12} md={9}>
+            <Typography m={1} variant="h6" gutterBottom>
+              Create Menu of {this.state.formValuesOfShop.shopName}
+            </Typography>
 
-              <MenuForm />
-            </div>
-            <div className="col-8" >
-              <MenuTableForm onSubmit={this.onSubmit} />
-              <MenuTable value={this.state.formValues} />
-            </div>
-          </div>
-        </div>
-      </div>
-    )
+            <MenuTableForm onSubmit={this.onSubmit} />
+            <MenuTable value={this.state.formValues} />
+          </Grid>
+          <Grid item xs={12} md={12}>
+            <Typography align="center">
+              <Button
+                type="submit"
+                size="large"
+                variant="contained"
+                color="success"
+              >
+                Save
+              </Button>
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    );
   }
 }
 
-
-
-export default MenuCreate
+export default MenuCreate;
