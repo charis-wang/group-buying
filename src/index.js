1d0e04f69e0344fde9cee0720761a3cc61674dc1
chariwang <charis53086@gmail.com>
2023-02-09 16:26:28 +0800

    fix a bug

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
 import AppProvider from "./components/App";
-import { store } from "./store";
+import { store } from "./store.js";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
